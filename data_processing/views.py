9481b4e0a8e06fa62fd1e4ea50f768d6eb5081a4
“Archie <2898047h@student.gla.ac.uk>
2025-11-26 11:31:39 +0000

    Added view for the upload file page. The view is still missing functions that have not yet been made. Specifically, the function to process the data is commented out and the redirects have not been added.

--- data_processing/views.py
+++ data_processing/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import UploadFileForm
 
 # Create your views here.
+def upload_file(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            #TODO: Add function to process data with SpaCy
+            #TODO: Add redirect to upload form
+
+    else:
+        form = UploadFileForm()
+    #TODO: return render for the upload template
