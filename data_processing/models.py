bd62e6d002c9be66eb16c2dc1ed3fbee59ca4318
“Archie <2898047h@student.gla.ac.uk>
2026-01-12 21:48:45 +0000

    Created database fields required for graph
    Added tables for EntityLink. Other tables, specifically Entities, IntelligenceReport, and EntityIntelligenceReport, were copied from data_processing branch.

--- data_processing/models.py
+++ data_processing/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class IntelligenceReport(models.Model):
+     reportID = models.AutoField(primary_key = True)
+     fullReport = models.TextField()
+     intelligenceSource = models.TextField(null = True, blank = True)
+
+     def __str__(self):
+        return f"Report {self.pk}"
+
+class Entity(models.Model):
+     entityID = models.AutoField(primary_key = True)
+     name = models.CharField(max_length = 255)
+     type = models.CharField(max_length = 255)
+
+     def __str__(self):
+        return f"{self.name} ({self.type})"
+
+class EntityIntelligenceReport(models.Model):
+     entity = models.ForeignKey(Entity, on_delete = models.CASCADE, null = True)
+     report = models.ForeignKey(IntelligenceReport, on_delete = models.CASCADE, null = True)
+
+     class Meta:
+        unique_together = ("entity", "report")
+
+     def __str__(self):
+        return f"{self.entity} in {self.report}"
+
+class EntityLink(models.Model):
+    linkID = models.AutoField(primary_key=True)
+    entity_1 = models.ForeignKey(Entity, on_delete = models.CASCADE, null = True)
+    entity_2 = models.ForeignKey(Entity, on_delete = models.CASCADE, null = True)
+    intelligence_report = models.ForeignKey(IntelligenceReport, on_delete = models.CASCADE, null = True)
