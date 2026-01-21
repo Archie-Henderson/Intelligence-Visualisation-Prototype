40d371afa5da269c5b360f485d896a3db521803b
Archie Henderson <2898047h@student.gla.ac.uk>
2026-01-21 10:33:19 +0000

    Revert "Merge branch 'file-upload' into 'main'"
    This reverts merge request !4

--- data_processing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from data_processing import views
-
-app_name = 'data_processing'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('upload/', views.upload, name='upload'),
-]
