b624d1d7bbf5f0457e3e93a6f981e0b892b10223
“Archie <2898047h@student.gla.ac.uk>
2026-03-04 12:47:09 +0000

    Made Filter Buttons Work Added a <form> tag to the graph html page to link the existing buttons to the form used for filtering.

--- data_visualisation/forms.py
+++ data_visualisation/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class FiltersForm(forms.Form):
     node_id = forms.IntegerField(required=False, label="Entity ID:")
     entity_type = forms.ChoiceField(required=False, label="Entity Type:", 
-        choices={"PE":"Person", "ORG":"Organisation", "LO":"Location", "TEL":"Telecom", "VE":"Vehicle"})
+        choices={"person":"Person", "organisation":"Organisation", "location":"Location", "telecom":"Telecom", "vehicle":"Vehicle"})
     entity_name = forms.CharField(required=False, label="Entity Name Includes:")
     report_id = forms.IntegerField(required=False, label="Report ID:")
     creation_date_start = forms.DateTimeField(required=False, label="Created After:")
