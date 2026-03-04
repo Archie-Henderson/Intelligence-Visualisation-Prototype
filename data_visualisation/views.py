511d8144da68b8fb95b8e972575fbfdd8a4ee962
“Archie <2898047h@student.gla.ac.uk>
2026-03-04 10:47:16 +0000

    Added Filtering Logic Currently implemented filters: entityID - walks tree connected to the entityID entity_type - filters entity by type. Case sensitive. entity_name - filters for names containing the inputted starting report_id - filters for entities that have an entry in EntityIntelligenceReport that also includes the entered report_id creation date filter logic added but commented out due to the model currently not having that field

--- data_visualisation/views.py
+++ data_visualisation/views.py
@@ -2,39 +2,83 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from data_processing.models import *
+from data_visualisation.forms import *
+from django.db.models.lookups import GreaterThan, LessThan
 
 import os
 import json
 
 # Create your views here.
 def graph_view(request):
+    filter_form = FiltersForm()
+
     ents = Entity.objects.all()
     links = EntityLink.objects.all()
     context = {'unlinked':[],
                'linked':[],
-               "cur_url": reverse('data_visualisation:graph')}
+               "cur_url": reverse('data_visualisation:graph'),
+               "form":filter_form}
     
     json_path = os.path.abspath("./static/graph_data.json")
 
     data = {'nodes':[],
             'links':[]}
-    
+
+    if request.method == "POST":
+        filter_form = FiltersForm(request.POST)
+
+        if filter_form.is_valid():
+            filters = filter_form.cleaned_data
+
+            if filters['node_id'] != None:
+                ents = ents.filter(entityID__in = walk_tree(filters['node_id']))
+            if filters['entity_type'] != "":
+                ents = ents.filter(type = filters['entity_type'])
+            if filters['entity_name'] != "":
+                ents = ents.filter(name__contains = filters['entity_name'])
+            # if filters['creation_date_end'] != None:
+            #     ents.filter(entry_date__lt = filters['creation_date_end'])
+            # if filters['creation_date_start'] != None:
+            #     ents.filter(entry_date__gt = filters['creation_date_start'])
+            if filters['report_id'] != None:
+                ents = ents.filter(entityID__in = EntityIntelligenceReport.objects.filter(report_id = filters['report_id']).values_list())
+
     for ent in ents:
         if EntityLink.objects.filter(entity_1 = ent).count() +  EntityLink.objects.filter(entity_2 = ent).count() == 0:
             context['unlinked'].append({'id':ent.entityID, 'name':ent.name})
-            print("Unlinked: " + str(ent.entityID) + ", " + ent.name)
         else:
             data['nodes'].append({'id':ent.entityID, 'name':ent.name})
             context['linked'].append({'id':ent.entityID, 'name':ent.name})
     
     for link in links:
-        data['links'].append({'source':link.entity_1.entityID, 'target':link.entity_2.entityID})
+        if link.entity_1 in ents and link.entity_2 in ents:
+            data['links'].append({'source':link.entity_1.entityID, 'target':link.entity_2.entityID})
 
-    print(json_path)
     with open(json_path, "w") as f:
         json.dump(data, f)
 
     return render(request, 'data_visualisation/graph.html', context = context)
+
+def get_report_entities(report_id):
+    ents = set()
+
+
+def walk_tree(ent_id):
+    nodes = [Entity.objects.get(entityID = ent_id)]
+    discovered_nodes = {Entity.objects.get(entityID = ent_id).entityID}
+
+    while len(nodes) > 0:
+        node = nodes.pop()
+        for ent in EntityLink.objects.filter(entity_1 = node.entityID).union(EntityLink.objects.filter(entity_2 = node.entityID)):
+            if ent.entity_1.entityID not in discovered_nodes:
+                discovered_nodes.add(ent.entity_1.entityID)
+                nodes.append(ent.entity_1)
+            elif ent.entity_2.entityID not in discovered_nodes:
+                discovered_nodes.add(ent.entity_2.entityID)
+                nodes.append(ent.entity_2)
+
+    return discovered_nodes
+            
 
 def entity_details(request, ent_id):
     entity = Entity.objects.get(entityID = ent_id)
