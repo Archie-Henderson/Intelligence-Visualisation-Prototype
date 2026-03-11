76f128a39bb54827d174a3f7b87d1fc4db41ed04
Azra Miray Atik <2890296a@student.gla.ac.uk>
2026-03-11 15:04:02 +0000

    Fix graph filtering by entity type

--- data_visualisation/forms.py
+++ data_visualisation/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from data_processing.models import Entity
 
 class FiltersForm(forms.Form):
     form_behaviour = forms.BooleanField(required=False)
     node_id = forms.IntegerField(required=False, label="Entity ID:")
     entity_type = forms.ChoiceField(required=False, label="Entity Type:", 
-        choices={"person":"Person", "organisation":"Organisation", "location":"Location", "telecom":"Telecom", "vehicle":"Vehicle"})
+        choices=[("", "---------")] + Entity.ENTITY_TYPES)
     entity_name = forms.CharField(required=False, label="Entity Name Includes:")
     report_id = forms.IntegerField(required=False, label="Report ID:")
     creation_date_start = forms.DateTimeField(required=False, label="Created After:")
--- data_visualisation/views.py
+++ data_visualisation/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.http import HttpResponse
 from data_processing.models import *
 from data_visualisation.forms import *
+from django.db.models import Count
 from django.db.models.lookups import GreaterThan, LessThan
 
 import os
@@ -10,61 +11,59 @@ import json
 
 # Create your views here.
 def graph_view(request):
-    filter_form = FiltersForm()
+    form_data = request.POST if request.method == "POST" else request.GET
+    filter_form = FiltersForm(form_data or None)
 
     ents = Entity.objects.all()
     filtered_ents = Entity.objects.all()
     links = EntityLink.objects.all()
-    filter_data = False
     filtering = False
-    context = {'unlinked':[],
-               'linked':[],
-               "cur_url": reverse('data_visualisation:graph'),
-               "form":filter_form}
-    
+
+    context = {
+        'unlinked': [],
+        'linked': [],
+        "cur_url": reverse('data_visualisation:graph'),
+        "form": filter_form,
+    }
+
     json_path = os.path.abspath("./static/graph_data.json")
+    data = {'nodes': [], 'links': []}
 
-    data = {'nodes':[],
-            'links':[]}
+    if filter_form.is_valid():
+        filters = filter_form.cleaned_data
 
-    if request.method == "POST":
-        filter_form = FiltersForm(request.POST)
+        if filters["node_id"] is not None:
+            filtered_ents = filtered_ents.filter(entityID__in=walk_tree(filters["node_id"]))
+            filtering = True
 
-        if filter_form.is_valid():
-            filters = filter_form.cleaned_data
+        if filters["entity_type"]:
+            filtered_ents = filtered_ents.filter(type=filters["entity_type"])
+            filtering = True
 
-            if filters['node_id'] != None:
-                filtered_ents = filtered_ents.filter(entityID__in = walk_tree(filters['node_id']))
-                filtering = True
-            if filters['entity_type'] != "":
-                filtered_ents = filtered_ents.filter(type = filters['entity_type'])
-                filtering = True
-            if filters['entity_name'] != "":
-                filtered_ents = filtered_ents.filter(name__contains = filters['entity_name'])
-                filtering = True
-            # if filters['creation_date_end'] != None:
-            #     filtered_ents = filtered_ents.filter(entry_date__lt = filters['creation_date_end'])
-            #     filtering = True
-            # if filters['creation_date_start'] != None:
-            #     filtered_ents = filtered_ents.filter(entry_date__gt = filters['creation_date_start'])
-            #     filtering = True
-            if filters['report_id'] != None:
-                filtered_ents = filtered_ents.filter(entityID__in = EntityIntelligenceReport.objects.filter(report_id = filters['report_id']).values_list())
-                filtering = True
-            if filters['form_behaviour']:
-                filter_data = True
+        if filters["entity_name"]:
+            filtered_ents = filtered_ents.filter(name__icontains=filters["entity_name"])
+            filtering = True
 
-    if filter_data:
+        if filters["report_id"] is not None:
+            report_entity_ids = (
+                EntityIntelligenceReport.objects
+                .filter(report_id=filters["report_id"], isDeleted=False, entity__isDeleted=False)
+                .values_list("entity_id", flat=True)
+            )
+            filtered_ents = filtered_ents.filter(entityID__in=report_entity_ids)
+            filtering = True
+
+    if filtering:
         data = add_data(filtered_ents, True, links, data)
     else:
         data = add_data(ents, False, links, data)
 
-    context = add_context(filtered_ents, filtering, context)
+    context = add_context(filtered_ents if filtering else ents, filtering, context)
 
     with open(json_path, "w") as f:
         json.dump(data, f)
 
-    return render(request, 'data_visualisation/graph.html', context = context)
+    return render(request, 'data_visualisation/graph.html', context=context)
 
 def add_context(ents, filtering, context):
     for ent in ents:
@@ -75,13 +74,34 @@ def add_context(ents, filtering, context):
     return context
 
 def add_data(ents, filtering, links, data):
+    # How many times each entity is mentioned across all reports (non-deleted)
+    mention_counts_qs = (
+        EntityIntelligenceReport.objects
+        .filter(entity__in=ents, isDeleted=False, entity__isDeleted=False)
+        .values("entity_id")
+        .annotate(count=Count("id"))
+    )
+    mention_counts = {row["entity_id"]: row["count"] for row in mention_counts_qs}
+
     for ent in ents:
-        if filtering or EntityLink.objects.filter(entity1 = ent).count() +  EntityLink.objects.filter(entity2 = ent).count() > 0:
-            data['nodes'].append({'id':ent.entityID, 'name':ent.name})
+        if filtering or EntityLink.objects.filter(entity1=ent).count() + EntityLink.objects.filter(entity2=ent).count() > 0:
+            freq = mention_counts.get(ent.entityID, 0)
+            data["nodes"].append({"id": ent.entityID, "name": ent.name, "freq": freq})
 
     for link in links:
         if link.entity1 in ents and link.entity2 in ents:
-            data['links'].append({'source':link.entity1.entityID, 'target':link.entity2.entityID})
+            level = link.relationLevel if link.relationLevel is not None else 1
+            freq1 = mention_counts.get(link.entity1_id, 0)
+            freq2 = mention_counts.get(link.entity2_id, 0)
+            importance = freq1 + freq2
+            data["links"].append(
+                {
+                    "source": link.entity1.entityID,
+                    "target": link.entity2.entityID,
+                    "level": level,
+                    "importance": importance,
+                }
+            )
         
     return data
 
@@ -103,19 +123,39 @@ def walk_tree(ent_id):
             
 
 def entity_details(request, ent_id):
-    entity = Entity.objects.get(entityID = ent_id)
-    reports=[]
-    for entity_intelligence_report in EntityIntelligenceReport.objects.filter(entity = entity):
-        reports.append(entity_intelligence_report.report)
+    entity = Entity.objects.get(entityID=ent_id)
 
-    links = []
+    # All reports where this entity is mentioned (respecting soft delete flags)
+    report_links = []
+    eir_qs = (
+        EntityIntelligenceReport.objects
+        .select_related("report", "entity")
+        .filter(entity=entity, isDeleted=False, report__isDeleted=False)
+        .order_by("report__reportID")
+    )
 
-    for link in EntityLink.objects.filter(entity1 = entity):
-        links.append({'other_ent':link.entity2.entityID, 'report':link.reportLink})
+    for eir in eir_qs:
+        report = eir.report
+        # All other entities in the same report
+        other_entities = (
+            Entity.objects.filter(
+                entityintelligencereport__report=report,
+                entityintelligencereport__isDeleted=False,
+                isDeleted=False,
+            )
+            .exclude(entityID=entity.entityID)
+            .distinct()
+            .order_by("name")
+        )
+        report_links.append(
+            {
+                "report": report,
+                "other_entities": list(other_entities),
+            }
+        )
 
-    for link in EntityLink.objects.filter(entity2 = entity):
-        links.append({'other_ent':link.entity1, 'report':link.reportLink})
-
-    print(entity, reports, links)
-    context = {'entity':entity, 'reports':reports, 'links':links}
-    return render(request, 'data_visualisation/entity_details.html', context=context)
+    context = {
+        "entity": entity,
+        "report_links": report_links,
+    }
+    return render(request, "data_visualisation/entity_details.html", context=context)
